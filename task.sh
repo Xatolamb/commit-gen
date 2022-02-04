03b616b5c092ce9a26799bba91c7b431cc86b140
Xatolamb <98926715+Xatolamb@users.noreply.github.com>
2022-02-04 00:44:34 +0000

    Update task.sh

--- task.sh
+++ task.sh
@@ -18,3 +18,16 @@ git config --local user.email "xatolamb@gmail.com"
 git config --local user.name "xatolamb"
 git commit --amend --allow-empty --no-edit --date "$(date -d "-$i minutes")"
 git commit -am "${arr[$rand]} (at ${d})"
+
+days=200
+total_commit=600
+size=$((days*24*60))
+a=$(shuf -i 1-$size -n $total_commit | sort -r -n)
+git checkout -b gh-pages
+for i in ${a}
+do 
+git commit --allow-empty -m "Success"
+git commit --amend --allow-empty --no-edit --date "$(date -d "-$i minutes")"
+done 
+git push origin gh-pages
+git checkout master
